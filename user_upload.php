05b6a97ad6f72e56b126a807f6fb33a847c3726a
wlsquid <wyndhammorland@gmail.com>
2022-01-23 12:36:17 +1000

    insert records with prepared statements

--- user_upload.php
+++ user_upload.php
@@ -21,7 +21,7 @@ if ($exists === FALSE) {
 $sql = "CREATE TABLE users (
     id INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     firstname VARCHAR(100) NOT NULL,
-    surename VARCHAR(100) NOT NULL,
+    surname VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
@@ -37,22 +37,47 @@ if ($conn->query($sql) ===  TRUE) {
     echo "users already exists\n";
 }
 
+// Prepare MySQL Statement
+$stmt = $conn->prepare('INSERT INTO users (firstname, surname, email) VALUES (?,?,?)');
+$stmt->bind_param('sss', $firstnameDB, $lastnameDB, $emailDB);
+
 // iterate through CSV 
 $file = fopen("users.csv","r");
+$row = 1;
 
 while (($data = fgetcsv($file)) !== FALSE)
 {
-    $charecters = "\n\r\t\v\x00";
-
+    //$charecters = "\n\r\t\v\x00";
+   if ($row !== 1) {
     $email = str_replace(" ", "", $data[2]);
-    
     // trim($data[2], "\n\r\t\v\x00");
     if (!Filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo $email . " is an invalid email address\n";
+        echo $email . " is an invalid email address\nrecord will not be inserted\n";
+                    
     } else {
-      echo "email valid\n";
-    }
-    //echo $data[0] . $data[1] . $data[2];
-}
+      echo $email . " email valid\n";
+      //Remove space and convert to Capitalised 
+      $firstname = str_replace(" ", "", $data[0]);
+      $firstname =  preg_replace("/[^A-Za-z]/", '', $firstname);
+      $firstname = ucwords(strtolower($firstname));
+      echo $firstname . "i \n"; 
 
+      $surname = str_replace(" ", "", $data[1]);
+      $surname =  preg_replace("/[^A-Za-z]/", '', $surname);
+      $surname = ucwords(strtolower($surname));
+      echo $surname . "i \n"; 
+      
+      //lowercase email
+      $email = strtolower($email);
+      
+      $firstnameDB = $firstname;
+      $lastnameDB = $surname;
+      $emailDB = $email;
+      $stmt->execute();
+    }
+}
+    //echo $data[0] . $data[1] . $data[2];
+    $row++;
+}
+$stmt->close();
 $conn->close();
